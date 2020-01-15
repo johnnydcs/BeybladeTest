9caacf7785ccd5b8b5fe4cd6d32100c9e322c2ff
Johnny <johnnydcs@yahoo.com>
2020-01-14 20:02:54 -0800

    Lots of additions
    Found free SFX assets for:  collision sparks, destruction lightning, trails, and others.
    BeybladeMovement handles the random speed range a top can have, what SFX happens on collision, health, and damage.
    Tops are restrained from rotating in the X/Z directions.
    Constant force values for X/Z are changed in the script.
    Sparks SFX shown when colliding.
    Tops are destroyed after reaching 0 or below health and show lightning SFX upon death.
    A top's movement and overall damage power is determined randomly, depending on the min/max constant force range.

--- Assets/BeybladeMovement.cs
+++ Assets/BeybladeMovement.cs
@@ -4,21 +4,91 @@ using UnityEngine;
 
 public class BeybladeMovement : MonoBehaviour
 {
+    public GameObject sparksPrefab;
+    public GameObject playerObject;
+    public GameObject lightningPrefab;
+
     public float moveSpeed = 1f;
-    public float rotationSpeed = 1f;
+    public float attackDmg = 5;
+    public float currentPower = 5;
+    public float health = 2000;
 
-    public float xAngle = 0f, yAngle = 10.0f, zAngle = 0f;
-    public float xConForce, yConForce = -5, zConForce;
+    public float xAngle = 0f;
+    public float yAngle = 10.0f;
+    public float zAngle = 0f;
 
-    public float conForceUpper = 50;
-    public float conForceLower = -50;
+    public float xConForce;
+    public float yConForce = -5;
+    public float zConForce;
+
+    public float conForceMax = 125;
+    public float conForceMin = -125;
+
     // Update is called once per frame
     void Update()
     {
-        xConForce = Random.Range(conForceLower, conForceUpper);
-        zConForce = Random.Range(conForceLower, conForceUpper);
+        if (health <= 0)
+        {
+            // show lightning sfx before death
+            GameObject lightningSfx = Instantiate(lightningPrefab, transform.position, transform.rotation);
+            Destroy(lightningSfx, .5f);
+
+            // destroy beyblade
+            Destroy(playerObject, 1); // not: Destroy(this, 1); which deletes BeybladeMovement script
+        }
+
+        setAttackPower();
+
+        xConForce = Random.Range(conForceMin, conForceMax);
+        zConForce = Random.Range(conForceMin, conForceMax);
+
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        GetComponent<ConstantForce>().force = new Vector3(xConForce, yConForce, zConForce);
         transform.Rotate(xAngle, yAngle, xAngle, Space.Self);
+
+        GetComponent<ConstantForce>().force = new Vector3(xConForce, yConForce, zConForce);
+
+        if (conForceMax < 0)
+            conForceMax = 0;
+
+        if (conForceMin > 0)
+            conForceMin = 0;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            takeDamage(collision.gameObject.GetComponent<BeybladeMovement>().currentPower);
+            GameObject sparksOnContact = Instantiate(sparksPrefab, transform.position, transform.rotation);
+            Destroy(sparksOnContact, .5f);
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "TopOfArena")
+        {
+            attackDmg = 15.0f;
+        }
+
+        else if (collision.gameObject.name == "MiddleOfArena")
+        {
+            attackDmg = 10.0f;
+        }
+
+        else if (collision.gameObject.name == "BottomOfArena")
+        {
+            attackDmg = 5.0f;
+        }
+    }
+
+    void setAttackPower()
+    {
+        currentPower = (attackDmg + xConForce + zConForce);
+    }
+
+    void takeDamage(float enemyDmg)
+    {
+        health -= enemyDmg;
     }
 }
